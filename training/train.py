aceed695b6937bdb9a4a6a3d65e09bd53829af7e
Jacopo Santolupo <jacoposantolupo@MacBook-Pro-di-Jacopo.local>
2025-11-06 11:43:59 +0100

    modified train.py

--- training/train.py
+++ training/train.py
@@ -2,7 +2,7 @@ import torch
 
 import wandb
 
-wandb.init(project="mldl_lab3", entity="tuo_username")
+wandb.init(project="project-skeleton", entity="s333730")
 
 def train(epoch, model, train_loader, criterion, optimizer, device):
     model.train()
